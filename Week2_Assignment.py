26ce9192c854e1d953295ddc9953a4593c6c9e22
BITOKCOLLINS <COLLINSKIPKOECHBITOK@GMAIL.COM>
2024-03-22 20:21:17 +0300

    Week 2 Assignment Python

---
+++ Week2_Assignment.py
@@ -0,0 +1,10 @@
+my_list = []
+my_list.extend([10,20,30,40])
+my_list.insert(1, 15)
+my_list.extend([50, 60, 70])
+my_list.pop() #Remove the last element from my_list.
+my_list.sort()
+index_of_30 = my_list.index(30)
+
+print(my_list)
+print(f"The index of 30 is: {index_of_30}")
